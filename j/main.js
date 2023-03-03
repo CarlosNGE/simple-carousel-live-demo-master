830d82e83db490d890f96d0294ea2f2a2c2441da
Juan Carlos <jcarlosnge@gmail>
2023-03-03 14:37:11 -0800

    first commit

---
+++ j/main.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', init);
+var myInterval = setInterval(changeSlide, 2000);
+
+function init() {
+  //create shortcut vars
+  const back_btn = document.querySelector(".back-btn");
+  const next_btn = document.querySelector(".next-btn");
+  const frame = document.querySelector(".frame");
+  const slides = frame.querySelectorAll("img");
+
+  //with JS active, hide all images
+  slides.forEach((slide) => {
+    slide.classList.add("hide");
+  });
+  
+  // show the first slide
+  slides[0].classList.remove("hide");
+  
+   next_btn.addEventListener("click",changeSlide);
+   back_btn.addEventListener("click", changeSlide);
+
+   
+}
+
+
+
+function changeSlide(e) {
+  if(e) {
+    // stop link from trying to reload page
+    e.preventDefault();
+    clearInterval(myInterval);
+
+  }
+    
+    //shortcut vars
+    const frame = document.querySelector(".frame");
+    const slides = frame.querySelectorAll("img");
+    let showing = document.querySelector(".current");
+    let nextUp = "";
+  
+    if(!e || e.target.className == 'next-btn') {
+      nextUp = showing.nextElementSibling;
+    } else {
+      nextUp = showing.previousElementSibling;
+    }
+    
+    // deactivate current image
+    showing.classList.add("hide");
+    showing.classList.remove("current");
+    
+    //make sure next image is there
+    if (!nextUp) {
+      nextUp = slides[slides.length - 1];
+    }
+  
+    if (nextUp.nodeName !== "IMG") {
+      nextUp = slides[0];
+    }
+  
+    // activate next image
+    nextUp.classList.remove("hide");
+    nextUp.classList.add("current");
+  }
